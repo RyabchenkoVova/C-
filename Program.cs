f5478ff3966123c477d6fb2cd9ad1bbcfc735323
RyabchenkoVova <61986198+RyabchenkoVova@users.noreply.github.com>
2020-03-09 21:38:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleappmaksym
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z;
+            x = Convert.ToDouble(Console.ReadLine());
+            y = Convert.ToDouble(Console.ReadLine());
+            z = Math.Pow(x - y, 2);
+
+            Console.WriteLine($"Z = {z}");
+
+
+        }
+    }
+}
